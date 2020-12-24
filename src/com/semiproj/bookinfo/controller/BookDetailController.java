bf638e7b1fa19fd9970c585de8f29078dc482326
Lee SoJeong <69043412+2020-07-31@users.noreply.github.com>
2020-12-25 00:57:02 +0900

    소정_댓글리스트_추가

--- src/com/semiproj/bookinfo/controller/BookDetailController.java
+++ src/com/semiproj/bookinfo/controller/BookDetailController.java
@@ -1,6 +1,7 @@
 package com.semiproj.bookinfo.controller;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import com.controller.Controller;
 import com.semiproj.bookinfo.model.BookInfoService;
 import com.semiproj.bookinfo.model.BookInfoVO;
+import com.semiproj.comments.model.CommentsService;
+import com.semiproj.comments.model.CommentsVO;
 import com.semiproj.writer.model.WriterService;
 import com.semiproj.writer.model.WriterVO;
 
@@ -36,11 +39,21 @@ public class BookDetailController implements Controller{
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		//3.
 		request.setAttribute("bookVo", vo);
 		request.setAttribute("writerVo", wVo);
 		
+		// 댓글 리스트 추가
+		CommentsService commService=new CommentsService();
+		List<CommentsVO> list=null;
+		try {
+			list=commService.selectAllCmt(Integer.parseInt(no));
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		request.setAttribute("commList", list);
 		//4.
 		return "/book/bookDetail.jsp";
 	}
