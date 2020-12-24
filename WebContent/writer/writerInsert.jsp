7b71c7d8c7201c03505a8ba0fbbf70f0f35b4ef2
sub0825 <tkgk40@gmail.com>
2020-12-24 19:10:50 +0900

    수정

---
+++ WebContent/writer/writerInsert.jsp
@@ -0,0 +1,64 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="../inc/top.jsp"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+<title>작가 대표작품</title>
+<meta charset="utf-8" />
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, user-scalable=no" />
+<link rel="stylesheet" href="../assets/css/main.css" />
+<noscript>
+	<link rel="stylesheet" href="../assets/css/noscript.css" />
+</noscript>
+<article id="banner"></article>
+<!-- Main -->
+<article id="main">
+
+
+<body class="right-sidebar is-preload">
+	<div id="page-wrapper">
+
+		<!-- One -->
+		<section class="wrapper style4 container">
+			<div class="row gtr-150">
+				<div class="col-8 col-12-narrower">
+
+					<h1>작가등록</h1>
+					<form action='<c:url value="/writer/writerInsert_ok.do" />' name="frmWrite"
+						method="post">
+						<label for="name">작가명</label><br> 
+						<input type="text" name="name" id="name"><br> 
+						<label for="intro">작가 소개글</label> <br> 
+						<input type="text"  name="intro" id="intro"><br><br> 
+						<label for="intro">작가 이미지</label><br>
+						<input type="file" value="파일 선택" name="file"/><br><br> 
+						
+						<input type="submit" value="전송">
+
+					</form>
+				</div>
+			</div>
+		</section>
+	</div>
+
+
+
+
+
+	<!-- Scripts -->
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/jquery.dropotron.min.js"></script>
+	<script src="../assets/js/jquery.scrolly.min.js"></script>
+	<script src="../assets/js/jquery.scrollgress.min.js"></script>
+	<script src="../assets/js/jquery.scrollex.min.js"></script>
+	<script src="../assets/js/browser.min.js"></script>
+	<script src="../assets/js/breakpoints.min.js"></script>
+	<script src="../assets/js/util.js"></script>
+	<script src="../assets/js/main.js"></script>
+</body>
+</html>
+<%@ include file="../inc/bottom.jsp"%>
+
+
--- src/com/semiproj/bookinfo/model/BookInfoDAO.java
+++ src/com/semiproj/bookinfo/model/BookInfoDAO.java
@@ -12,31 +12,31 @@ import com.semiproj.db.ConnectionPoolMgr;
 
 public class BookInfoDAO {
 	private ConnectionPoolMgr pool;
-	
+
 	public BookInfoDAO(){
 		pool=new ConnectionPoolMgr();
 	}
-	
+
 	public List<BookInfoVO> selectAll(String condition, String keyword) throws SQLException{
 		Connection con=null;
 		PreparedStatement ps=null;
 		ResultSet rs=null;
-		
+
 		List<BookInfoVO> list=new ArrayList<BookInfoVO>();
 		try {
 			con=pool.getConnection();
-			
+
 			String sql="select * from bookinfo";
 			if(keyword!=null && !keyword.isEmpty()) {
 				sql+=" where "+condition+" like '%'||?||'%'";
 			}
 			sql+=" order by no desc";
 			ps=con.prepareStatement(sql);
-			
+
 			if(keyword!=null && !keyword.isEmpty()) {
 				ps.setString(1, keyword);
 			}
-			
+
 			rs=ps.executeQuery();
 			while(rs.next()) {
 				int no=rs.getInt("no");
@@ -50,34 +50,34 @@ public class BookInfoDAO {
 				String coverFileName=rs.getString("coverFileName");
 				String txtOriginFileName=rs.getString("txtOriginFileName");
 				String coverOriginFileName=rs.getString("coverOriginFileName");
-				
+
 				BookInfoVO vo=new BookInfoVO(no, title, price, regdate, publisher, wrNo, content, txtFileName, coverFileName, txtOriginFileName, coverOriginFileName);
 				list.add(vo);
 			}
 			System.out.println("list.size="+list.size()
-					+", 매개변수 condition="+condition+", keyword="+keyword);
-			
+			+", 매개변수 condition="+condition+", keyword="+keyword);
+
 			return list;
 		}finally {
 			pool.dbClose(rs, ps, con);
 		}
 	}
-	
+
 	public BookInfoVO selectByNo(int no) throws SQLException{
 		Connection con=null;
 		PreparedStatement ps=null;
 		ResultSet rs=null;
-		
+
 		BookInfoVO vo=null;
 		try {
 			con=pool.getConnection();
-			
+
 			String sql="select * from bookinfo" + 
 					" where no=?";
 			ps=con.prepareStatement(sql);
-			
+
 			ps.setInt(1, no);
-			
+
 			rs=ps.executeQuery();
 			if(rs.next()) {
 				String title=rs.getString("title");
@@ -90,28 +90,28 @@ public class BookInfoDAO {
 				String coverFileName=rs.getString("coverFileName");
 				String txtOriginFileName=rs.getString("txtOriginFileName");
 				String coverOriginFileName=rs.getString("coverOriginFileName");
-				
+
 				vo=new BookInfoVO(no, title, price, regdate, publisher, wrNo, content, txtFileName, coverFileName, txtOriginFileName, coverOriginFileName);
 			}
 			System.out.println("번호로 조회 결과 vo="+vo+", 매개변수 no="+no);
-			
+
 			return vo;
 		}finally {
 			pool.dbClose(rs, ps, con);
 		}
 	}
-	
+
 	public int insertBook(BookInfoVO vo) throws SQLException{
 		Connection con=null;
 		PreparedStatement ps=null;
-		
+
 		try{
 			con=pool.getConnection();
-			
+
 			String sql="insert into bookinfo(no, title, price, publisher, wrno, content, txtfilename, coverfilename, txtoriginfilename, coveroriginfilename)" + 
 					" values(bookinfo_seq.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			ps=con.prepareStatement(sql);
-			
+
 			ps.setString(1, vo.getTitle());
 			ps.setInt(2, vo.getPrice());
 			ps.setString(3, vo.getPublisher());
@@ -121,13 +121,54 @@ public class BookInfoDAO {
 			ps.setString(7, vo.getCoverFileName());
 			ps.setString(8, vo.getTxtOriginFileName());
 			ps.setString(9, vo.getCoverOriginFileName());
-			
+
 			int cnt=ps.executeUpdate();
 			System.out.println("책 등록 결과 cnt="+cnt+", 매개변수 vo="+vo);
-			
+
 			return cnt;
 		}finally {
 			pool.dbClose(ps, con);
 		}
 	}
+
+	//작가번호로 책조회
+	public List<BookInfoVO> selectByWrno(int wrno) throws SQLException {
+		Connection con=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		List<BookInfoVO> list=new ArrayList<BookInfoVO>(); 
+
+		try {
+			con=pool.getConnection();
+
+			String sql="select * from bookinfo where wrno=?"
+					+ "	order by regdate desc";
+			ps=con.prepareStatement(sql);
+			ps.setInt(1, wrno);
+
+			rs=ps.executeQuery();
+			while(rs.next()) {
+				int no=rs.getInt("no");
+				String content=rs.getString("content");
+				String coverFileName=rs.getString("coverFileName");
+				String coverOriginFileName=rs.getString("coverOriginFileName");
+				int price=rs.getInt("price");
+				String publisher=rs.getString("publisher");
+				Timestamp regdate=rs.getTimestamp("regdate");
+				String title=rs.getString("title");
+				String txtFileName=rs.getString("txtFileName");
+				String txtOriginFileName=rs.getString("txtOriginFileName");
+
+				BookInfoVO vo=new BookInfoVO(no, title, price, regdate, publisher, wrno, content, txtFileName, coverFileName, txtOriginFileName, coverOriginFileName);
+				list.add(vo);
+
+			}
+			System.out.println("작가 책 조회 결과 list.size="+list.size()+", 매개변수 no="+wrno);
+			return list;
+
+		}finally {
+			pool.dbClose(rs, ps, con);
+		}
+	}
+
 }
