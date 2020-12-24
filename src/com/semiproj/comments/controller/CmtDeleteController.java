a5e40e00d7711cd212985a980fddb3705cc982bf
Lee SoJeong <69043412+2020-07-31@users.noreply.github.com>
2020-12-25 01:47:26 +0900

    소정_댓글삭제Controller_추가
    prop에 추가
    /comments/comments_delete.do=com.semiproj.comments.controller.CmtDeleteController

---
+++ src/com/semiproj/comments/controller/CmtDeleteController.java
@@ -0,0 +1,41 @@
+package com.semiproj.comments.controller;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.controller.Controller;
+import com.semiproj.comments.model.CommentsService;
+
+public class CmtDeleteController implements Controller{
+
+	@Override
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		/*
+		  	/comments/comments_delete.do => CmtDeleteController
+		  	  =>/comments/comments_detail.do로 리다이렉트
+		 */
+		String no=request.getParameter("no");
+		System.out.println("deleteconteriller : no="+no);
+		CommentsService cmtservice=new CommentsService();
+		try {
+			int cnt=cmtservice.deleteCmt(Integer.parseInt(no));
+			if(cnt>0) {
+				System.out.println("삭제 성공");
+			}else {
+				System.out.println("삭제 실패");
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return "/comments/comments_detail.do?no="+no;
+	}
+
+	@Override
+	public boolean isRedirect() {
+		return false;
+	}
+
+}
